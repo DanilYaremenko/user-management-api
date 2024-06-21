336a8657b78ddf7f9b592cd6ca43459a517caac6
DanilYaremenko <danilyaremenko@gmail.com>
2024-06-21 05:30:55 +0300

    feat: expand error handling for 'Bad Request' in response interceptor

--- src/common/response.interceptor.ts
+++ src/common/response.interceptor.ts
@@ -4,6 +4,7 @@ import {
   ExecutionContext,
   CallHandler,
   HttpException,
+  BadRequestException,
 } from '@nestjs/common';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
@@ -17,6 +18,25 @@ export class ResponseInterceptor implements NestInterceptor {
       })),
       catchError((error) => {
         const { status, message } = error;
+
+        if (error instanceof BadRequestException) {
+          const response = error.getResponse();
+          const errors = Array.isArray(response['message'])
+            ? response['message']
+            : [response['message']];
+
+          return throwError(
+            () =>
+              new HttpException(
+                {
+                  message: errors,
+                  error: 'Bad Request',
+                  statusCode: 400,
+                },
+                400,
+              ),
+          );
+        }
 
         return throwError(
           () =>
